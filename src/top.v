be75e034cce337dad6e3799f1d141ff315b081ef
Zar Ni Tun <268078@vutbr.cz>
2026-04-08 11:27:35 +0200

    Update top.v

--- src/top.v
+++ src/top.v
@@ -2,7 +2,8 @@ module top (
     input clk,
     input btnd, // Start/ Stop
     input btnu, // Reset
-    input btnr, // Lap
+    input btnr, // Save current time as lap
+    input btnl, // Browse display: saved laps/ live time
     output [6:0] seg,
     output [7:0] an,
     output dp
