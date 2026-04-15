752f4f3974ad58803709120b60b01af91247297d
Zar Ni Tun <268078@vutbr.cz>
2026-04-15 10:33:07 +0200

    Update stopwatch_counter.v

--- src/stopwatch_counter.v
+++ src/stopwatch_counter.v
@@ -1,2 +1,21 @@
-module stopwatch_counter;
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+
+module counter # ( 
+    parameter N = 4
+)(
+    input i_clk,
+    input i_rst,
+    input i_en,
+    output reg [N-1:0] o_cnt
+    );
+    
+    always @(posedge i_clk) begin
+        if (i_rst) begin
+            o_cnt <= 0;
+        end
+        else if (i_en) begin   
+            o_cnt <= o_cnt + 1'b1;
+        end
+    end
 endmodule
